1be0e2a5b49a2f3072b0da4520e1d7ca22ba0dfd
Satnam Singh <satnam@localhost.localdomain>
2020-04-03 17:48:33 -0500

    Sample to add queueReceiver

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Azure.ServiceBus;
+
+namespace subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Azure.ServiceBus.QueueClient queueClient = new QueueClient("Endpoint=sb://singheveloper-azure.servicebus.windows.net/;SharedAccessKeyName=testQueueMng;SharedAccessKey=ASUse6UlF71pw9LZCOfAcuexfq+pwfiVpvfQPcxzNvk=;","testqueue");
+            queueClient.RegisterMessageHandler(
+                    async (msg,cancelationToken) => {
+                            var body = System.Text.Encoding.UTF8.GetString(msg.Body);
+                            Console.WriteLine(body);
+            },async exception => {
+                Console.WriteLine("Error ");
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
